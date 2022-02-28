5f95c0730e1653a1abde96885695194e16a41552
hakan yalcin <tchakanyalcin>
2022-02-28 10:26:37 +0300

    initial

--- env-extractor.js
+++ env-extractor.js
@@ -1,15 +1,56 @@
 var fs = require('fs');
+var path = require('path');
+let workingPath = process.argv.slice(2)[0];
+var envArr = [];
 
-//const directory = process.argv.slice(2);
-//console.log("directory name is:" + directory[0]);
-//var files = fs.readdirSync(directory[0]);
 
-var file = fs.readFileSync('dummy.txt');
+let walk = function(dir, done) {
+var results = [];
+fs.readdir(dir, function(err, list) {
+  if (err) return done(err);
+  var i = 0;
+  (function next() {
+    var file = list[i++];
+    if (!file) return done(null, results);
+    file = path.resolve(dir, file);
+    
 
-FILE_LOCATION = 'dummy.txt';
-fs.readFile(FILE_LOCATION, function (err, data) {
+    fs.stat(file, function(err, stat) {
+      if (stat && stat.isDirectory()) {
+        if(dir !== `${workingPath}/node-modules`) {
+          walk(file, function(err, res) {
+            next();
+          });
+        }
+      } else {
+        fs.readFile(file, function (err, file) {
+          if (err) throw err;
+          var str = file.toString();
+          const regexp = new RegExp('process.env.','g');
+          const matches = str.matchAll(regexp);
+
+            for (const match of matches) {
+            let startIndex = match.index + match[0].length ;              
+            let envVar = str.slice(startIndex, str.indexOf((' '), startIndex));      
+            var result = envVar.split(/\W/)[0];
+            envArr.push(result);
+          }
+        });         
+        next();
+      }
+    });
+
+
+  })();
+});
+  
+};
+
+
+
+
+  walk(workingPath, function(err, results) {
     if (err) throw err;
-    if(data.includes('process.env.')){
-     console.log(data)
-    } else console.log('not found');
+    const uniqueEnv = envArr.filter((x, i, a) => a.indexOf(x) == i);
+    console.log(uniqueEnv);
   });
